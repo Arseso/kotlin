d379732964bfaa580503ce6484adc232a8cc85ba
Artem <92620390+Arseso@users.noreply.github.com>
2025-03-27 13:56:42 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,13 +11,19 @@ repositories {
 }
 
 dependencies {
-        implementation("com.fasterxml.jackson.core:jackson-core:2.17.2")
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.2")
+    // JSON
+    implementation("com.fasterxml.jackson.core:jackson-core:2.17.2")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.2")
+    // CSV
     implementation("com.jsoizo:kotlin-csv-jvm:1.10.0")
+    // HTTP4K
     implementation("org.http4k:http4k-core:5.32.0.0")
     implementation("org.http4k:http4k-client-apache:5.32.0.0")
     implementation("org.http4k:http4k-server-netty:5.32.0.0")
     implementation("org.http4k:http4k-format-jackson:5.32.0.0")
+
+    //JCommander
+    implementation("org.jcommander:jcommander:2.0")
     testImplementation(kotlin("test"))
 }
 
